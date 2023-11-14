720bb9f16d22c9bf85d4c181db12a9f37d02beb1
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-14 16:04:12 -0500

    Updated App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,35 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+// Import your components here
+import SearchBooks from './components/SearchBooks';
+import SavedBooks from './components/SavedBooks';
 import Navbar from './components/Navbar';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 
-function App() {
+const client = new ApolloClient({
+  uri: '/graphql', // Update with your GraphQL server endpoint
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/signup" component={SignupForm} />
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
